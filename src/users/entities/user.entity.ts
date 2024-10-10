e7eeaba7703c72f915e425d6ca3a916b39f13a44
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-10-10 15:08:32 -0300

    configurando o banco com a entidade users

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,43 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nome: string;
+  
+  @Column()
+  cpf: string;
+
+  @Column()
+  email: string;
+  
+  @Column()
+  funcao: string;
+  
+  @Column()
+  telefone: string;
+  
+  @Column()
+  cep: string;
+  
+  @Column()
+  cidade: string;
+  
+  @Column()
+  rua: string;
+  
+  @Column()
+  bairro: string;
+  
+  @Column()
+  usuario: string;
+  
+  @Column()
+  senha: string; // deve usar hash
+  
+  @Column()
+  nivelDeAcesso: number;
+}
