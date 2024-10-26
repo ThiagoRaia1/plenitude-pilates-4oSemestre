8a9767783cba11d7e8a68e923e3a9e3e67177a1e
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-10-26 10:21:39 -0300

    "create" e "findAll" para entidade "usuario"

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { Usuario } from './entities/usuario.entity';
 
 @Injectable()
 export class UsuarioService {
+  constructor(
+    @InjectRepository(Usuario)
+    private usuarioRepository: Repository<Usuario>,
+  ) {}
+
   create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+    const usuario = this.usuarioRepository.create(createUsuarioDto)
+    return this.usuarioRepository.save(usuario)
   }
 
   findAll() {
-    return `This action returns all usuario`;
+    return this.usuarioRepository.find();
   }
 
   findOne(id: number) {
