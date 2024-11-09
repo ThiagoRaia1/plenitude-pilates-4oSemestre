f695a7339dc866b9d1969a16e57add677eae2a21
Bryan Vinicius Gomes <gomesbryan754@gmail.com>
2024-11-08 21:38:46 -0300

    CRUD para Aluno

--- src/alunos/alunos.module.ts
+++ src/alunos/alunos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AlunosService } from './alunos.service';
 import { AlunosController } from './alunos.controller';
+import { Aluno } from './entities/aluno.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Aluno])],
   controllers: [AlunosController],
   providers: [AlunosService],
 })
