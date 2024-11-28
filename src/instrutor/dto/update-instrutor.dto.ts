5701a2823e51570b0c540421d387645a051f8c78
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-11-27 23:50:27 -0300

    update-instrutor.dto

--- src/instrutor/dto/update-instrutor.dto.ts
+++ src/instrutor/dto/update-instrutor.dto.ts
@@ -1,4 +1,42 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateInstrutorDto } from './create-instrutor.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateInstrutorDto extends PartialType(CreateInstrutorDto) {}
+export class UpdateInstrutorDto extends PartialType(CreateInstrutorDto) {
+    @IsOptional()
+    nome?: string;
+    
+    @IsOptional()
+    dataNascimento?: Date;
+
+    @IsOptional()
+    cpf?: string;
+  
+    @IsOptional()
+    rua?: string;
+
+    @IsOptional()
+    telefone?: string;
+
+    @IsOptional()
+    status?: string;
+
+    ultimaAlteracao: string;
+
+    dataUltimaAlteracao: Date;
+
+    @IsOptional()
+    numeroRua?: number;
+    
+    @IsOptional()
+    numeroCasa?: number;
+    
+    @IsOptional()
+    cep?: string;
+    
+    @IsOptional()
+    bairro?: string;
+    
+    @IsOptional()
+    cidade?: string;
+}
