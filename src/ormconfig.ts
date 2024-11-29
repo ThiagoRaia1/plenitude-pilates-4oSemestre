c33f394a0db70e3e79b2a1db36b1aee24391e213
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2024-11-29 19:27:17 -0300

    Uso do sqlite para deploy no render

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,16 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const config: TypeOrmModuleOptions = {
-  type: 'mssql',
-  host: 'localhost',
-  port: 1433,
-  username: 'sa',
-  password: 'thiago',
-  database: 'PlenitudePilates',
+  type: 'sqlite',
+  database: 'database.sqlite',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
-  options: {
-    encrypt: true,
-    trustServerCertificate: true, // Ignora problemas com certificados autoassinados, nao use em producao
-  },
 };
